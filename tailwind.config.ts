aa91243310645500189db717910f1c0a8ae1d760
Bakate <bakateba@gmail.com>
2025-02-14 12:03:10 +0100

    feat: add postgres dependency to package.json

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
   content: [
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
+    "./src/presentation/**/*.{ts,tsx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
