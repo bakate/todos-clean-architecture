f0e6f31bf63aee5db00f8e9343410aa8cf543de7
Bakate <bakateba@gmail.com>
2025-02-15 12:46:15 +0100

    feat(todos): update TodoList component to use Checkbox for task completion and enhance button actions with icons

--- src/presentation/features/todos/components/TodoList.tsx
+++ src/presentation/features/todos/components/TodoList.tsx
@@ -4,6 +4,8 @@ import { Button } from "@heroui/button";
 import { Card, CardBody, CardFooter, CardHeader } from "@heroui/card";
 import { TodoViewModel } from "../presenters/todo.presenter";
 import { formatDate } from "@/src/presentation/shared/utils/date";
+import { Checkbox } from "@heroui/react";
+import { LuPencil, LuTrash2 } from "react-icons/lu";
 
 interface TodoListProps {
   todos: TodoViewModel[];
@@ -40,33 +42,18 @@ export function TodoList({
           <CardHeader className="min-h-[80px]">
             <div className="flex gap-2 flex-col h-full">
               <h2
-                className={`text-lg font-semibold leading-tight line-clamp-1 flex items-center gap-2 ${
-                  todo.completed ? "line-through text-gray-500" : ""
-                }`}
+                className={
+                  "text-lg font-semibold leading-tight line-clamp-1 flex items-center gap-2"
+                }
               >
-                <span
-                  className={`inline-flex items-center justify-center w-5 h-5 border-2 rounded ${
-                    todo.completed
-                      ? "border-success bg-success text-white"
-                      : "border-gray-300"
-                  }`}
+                <Checkbox
+                  defaultSelected={todo.completed}
+                  color={todo.completed ? "success" : "default"}
+                  lineThrough={todo.completed}
+                  onChange={() => onToggleComplete(todo)}
                 >
-                  {todo.completed && (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-3 h-3"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  )}
-                </span>
-                {todo.title}
+                  {todo.title}
+                </Checkbox>
               </h2>
               <p
                 className={`text-sm text-gray-500 line-clamp-2 ${
@@ -97,19 +84,17 @@ export function TodoList({
             <Button
               variant="bordered"
               size="sm"
-              color={todo.completed ? "warning" : "success"}
-              onPress={() => onToggleComplete(todo)}
+              onPress={() => onEdit(todo)}
+              endContent={<LuPencil />}
             >
-              {todo.completed ? "Annuler" : "Terminer"}
-            </Button>
-            <Button variant="bordered" size="sm" onPress={() => onEdit(todo)}>
               Edit
             </Button>
             <Button
-              variant="solid"
+              variant="bordered"
               color="danger"
               size="sm"
               onPress={() => onDelete(todo)}
+              endContent={<LuTrash2 />}
             >
               Delete
             </Button>
