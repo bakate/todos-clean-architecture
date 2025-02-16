bbd6ef2802a60e16d0bf79d476c86afd90a5a7a5
Bakate <bakateba@gmail.com>
2025-02-16 16:51:26 +0100

    refactor(todos): remove mock use cases and related entities for todos

--- src/application/use-cases/todo/__mocks__/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./create-todo.usecase";
-export * from "./get-todo.usecase";
-export * from "./list-todos.usecase";
-export * from "./update-todo.usecase";
-export * from "./delete-todo.usecase";
-export * from "./toggle-todo-complete.usecase";
